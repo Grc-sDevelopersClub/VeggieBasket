b2d88572ad31599919e55d96fe42c996233b7876
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-07-28 06:45:39 +0530

    #4fdpqp[REVIEW]Connected with EC2 mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const SERVER=process.env.SERVER;
+mongoose.connect('mongodb://'+SERVER+':27017/VeggieBasket', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("DB is now connected");// we're connected!
+});
 
 app.get("/", (req,res) => {
   res.send("Hey Veggie Finder");
