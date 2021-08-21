6637e6166fb2476adba6559c6e9511e2154753f5
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-08-21 15:59:33 +0530

    Cleaning client folder

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import styled from 'styled-components';
 
 
@@ -7,18 +6,7 @@ function App() {
     <div className="App">
     <Container>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
       </Container>
     </div>
--- Server/routes/account.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-let app = express.Router()
-
-app.get("/",(req,res)=>{
-    res.send("Welcome to your account")
-});
-
-module.exports= app
